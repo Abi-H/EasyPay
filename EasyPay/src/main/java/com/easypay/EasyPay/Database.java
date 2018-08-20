d17324c1c50a833116854f9c0008a4839a769ad1
abigail <abigail@DESKTOP-L5CID71>
2018-08-20 12:20:17 +1200

    Added Database.java and LoginService.java

--- EasyPay/src/main/java/com/easypay/EasyPay/Database.java
+++ EasyPay/src/main/java/com/easypay/EasyPay/Database.java
@@ -1,5 +1,64 @@
 package com.easypay.EasyPay;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Database {
 
+	String dbUrl;
+	String uname;
+	String password;
+	Connection conn;
+	ArrayList<String> fields;
+
+	public Database() throws ClassNotFoundException, SQLException {
+		this.dbUrl = "jdbc:mysql://localhost:3306/easypay";
+		this.uname = "root";
+		this.password = "";
+		fields = new ArrayList<>();
+		conn = DriverManager.getConnection(dbUrl, uname, password);	
+	}
+
+	public boolean checkCredentials(String user, String pass) {
+		try {
+			PreparedStatement preparedStmt = conn.prepareStatement("SELECT username,password FROM users WHERE username=? AND password=?");
+			preparedStmt.setString(1, user);
+			preparedStmt.setString(2, pass);
+			
+			ResultSet rs = preparedStmt.executeQuery();
+
+			if (rs.next()) {
+				return true;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;	
+	}
+	
+	public void getHistory(String username) {
+		try {
+			ArrayList<String> fields = new ArrayList<>();
+			String sql = "SELECT date, amount FROM transactions WHERE username=?";	
+			
+			PreparedStatement preparedStmt = conn.prepareStatement(sql);			
+			preparedStmt.setString(1, username);
+			
+			ResultSet rs = preparedStmt.executeQuery();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<String> getFields() {
+		return fields;
+	}
 }
+
--- EasyPay/src/main/java/com/easypay/EasyPay/LoginService.java
+++ EasyPay/src/main/java/com/easypay/EasyPay/LoginService.java
@@ -1,5 +1,45 @@
 package com.easypay.EasyPay;
 
-public class LoginService {
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.SQLException;
 
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+@Path("login")
+
+public class LoginService {
+	@Context
+	private ServletContext context;
+	
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	
+	public Response process(@QueryParam("username") String username, @QueryParam("password") String password)
+			throws ClassNotFoundException, SQLException, MalformedURLException, URISyntaxException {
+		Database db = new Database();
+		URI uri;
+		
+		if(db.checkCredentials(username, password)) {
+			String urlStr = "http://localhost:8080/CryptoMail" + "/dashboard.jsp" + "?name=" + username;
+			URL url= new URL(urlStr);			
+			uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+		} else {
+			uri = UriBuilder.fromPath(context.getContextPath() + "/login.jsp").build();
+		}
+		
+		return Response.seeOther(uri).build();
+	}
 }
