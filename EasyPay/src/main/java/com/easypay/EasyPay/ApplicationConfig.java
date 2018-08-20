ca0b96f672572cec611a041ce5aa2caa58a79183
abigail <abigail@DESKTOP-L5CID71>
2018-08-20 12:53:45 +1200

    Updated web.xml and pom.xml so the server runs.
    Added skeleton code for login.jsp to test

---
+++ EasyPay/src/main/java/com/easypay/EasyPay/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package com.easypay.EasyPay;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("easypay")
+public class ApplicationConfig extends Application {}
